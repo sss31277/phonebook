6484497de80b178b808b853257324d1a824fe572
黃士洋 <sss3127756@gmail.com>
2018-03-05 22:58:43 +0800

    Make entry smaller

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,10 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+ #define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];/*
+    char lastName[MAX_LAST_NAME_SIZE];
+    /*
     char firstName[16];
     char email[16];
     char phone[10];
@@ -16,9 +17,25 @@ typedef struct __PHONE_BOOK_ENTRY {
     char addr2[16];
     char city[16];
     char state[2];
-    char zip[5];*/
+    char zip[5];
+    */
+    struct Data *data;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
+
+typedef struct Data  {
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+} detail;
+
+
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
